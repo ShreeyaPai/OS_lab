7e49cd0c39c7dd948c63876762c1c090c7b01c93
Shreeya <121918827+ShreeyaPai@users.noreply.github.com>
2023-07-31 18:56:55 +0530

    Update RoundRobin.c
    Idle handling included

--- RoundRobin.c
+++ RoundRobin.c
@@ -1,10 +1,10 @@
 #include<stdlib.h>
 #include<stdio.h>
-
+#include<limits.h>
 
 typedef struct process
 {
-	int id,at,bt,ct,wt,tat,flag;
+	int id,at,bt,ct,wt,tat,visited,BT;
 }pro;
 
 pro p[10];
@@ -30,7 +30,7 @@ void sort(int n)
 
 void main()
 {
-	int n,tempBT[10],Ttat=0,Twt=0,completed=0,time=0,curI,quantum;
+	int n,Ttat=0,Twt=0,completed=0,time=0,curI,quantum;
 	printf("Enter the number of processes: ");
 	scanf("%d",&n);
 	printf("Enter the AT and BT of the processes\n");
@@ -38,22 +38,23 @@ void main()
 	{
 		p[i].id=i+1;
 		scanf("%d%d",&p[i].at,&p[i].bt);
-		tempBT[i]=p[i].bt;
-		p[i].flag=0;
+		p[i].BT=p[i].bt;
+		p[i].visited=0;
 	}
 	printf("Enter time quantum: ");
 	scanf("%d",&quantum);
 	
 	sort(n);
 	
-	int Q[10],front=0,rear=0;
-	Q[rear]=0;
-	p[0].flag=1;
+	int Q[100]={0},front=0,rear=-1;
+	Q[++rear]=0;
+	p[0].visited=1;
+	int count=1;
 	
 	while(completed!=n)
 	{
-		curI=Q[front];
-		front=(front+1)%n;
+		curI=Q[front++];
+		count--;
 		if(p[curI].bt>quantum)
 		{
 			time+=quantum;
@@ -65,34 +66,47 @@ void main()
 			time+=p[curI].bt;
 			printf(" | P%d (%d) %d | ",p[curI].id,p[curI].bt,time);
 			p[curI].bt=0;
-		}
-		for(int i=0;i<n && p[i].at<=time;i++)
-		{
-			if(i==curI || p[i].bt==0 || p[i].flag) continue;
-			rear=(rear+1)%n;
-			Q[rear]=i;
-			p[i].flag=1;
-		}
-		if(p[curI].bt!=0)
-		{
-			rear=(rear+1)%n;
-			Q[rear]=curI;
-		}
-		else
-		{
 			completed++;
 			p[curI].ct=time;
 			p[curI].tat=p[curI].ct-p[curI].at;
-			p[curI].wt=p[curI].tat-tempBT[curI];
+			p[curI].wt=p[curI].tat-p[curI].BT;
 			Ttat+=p[curI].tat;
 			Twt+=p[curI].wt;
 		}
+		for(int i=1;i<n;i++)
+		{
+			if(p[i].at<=time && !p[i].visited)
+			{
+			Q[++rear]=i;
+			p[i].visited=1;
+			count++;
+			}
+		}
+		if(p[curI].bt>0)
+		{
+			Q[++rear]=curI;
+			count++;
+		}
+		while(front>rear && completed<n) //or count==0
+		{
+			time++;
+			printf("|idle (1) %d",time);
+			for(int i=1;i<n;i++)
+			{
+				if(p[i].at<=time && !p[i].visited)
+				{
+					Q[++rear]=i;
+					p[i].visited=1;
+					count++;
+				}
+			}
+		}
 	}
 	printf("\n");
-	printf("PID   AT   BT   CT\tWT\tTAT\n");
+	printf("PID\tAT\tBT\tCT\tWT\tTAT\n");
 	for(int i=0;i<n;i++)
 	{
-		printf("%d   %d   %d   %d\t%d\t%d\n",p[i].id,p[i].at,tempBT[i],p[i].ct,p[i].wt,p[i].tat);
+		printf("%d\t%d\t%d\t%d\t%d\t%d\n",p[i].id,p[i].at,p[i].BT,p[i].ct,p[i].wt,p[i].tat);
 	}
 	printf("Avg TAT %f\n",(float)Ttat/n);
 	printf("Avg WT %f\n",(float)Twt/n);
